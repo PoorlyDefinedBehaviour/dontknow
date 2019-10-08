a253f0c09d054ebbb9b23c26790f5b16e7f1892b
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2019-10-07 23:59:21 -0300

    :heavy_plus_sign: adds compression/helmet as dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import load from "process-env-loader";
-load();
-import express from "express";
-import { Express } from "express-serve-static-core";
-import loadRoutes from "./utils/LoadRoutes";
+import "dotenv/config";
+import express, { Express } from "express";
+import loadRoutes from "./utils/load-routes";
 import cors from "cors";
-import rateLimit from "./config/RateLimit";
-import sessionHandler from "./config/Session";
-import globalExceptionHandler from "./http/middlewares/GlobalExceptionHandler";
+import rateLimit from "./config/express-rate-limit.config";
+import sessionHandler from "./config/express-session.config";
+import globalExceptionHandler from "./http/middlewares/global-exception-handler.middleware";
 import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+export const app: Express = express();
 
 async function main(): Promise<void> {
-  const app: Express = express();
   app.disable("X-Powered-By");
   app.use(morgan("combined"));
+  app.use(helmet());
+  app.use(compression());
   app.use(express.json());
   app.use(cors());
   app.use(rateLimit);
